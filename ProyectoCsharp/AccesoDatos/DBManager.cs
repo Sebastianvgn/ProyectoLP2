8a35a8682a07e9cba8b1c370cd51d2b0ec1baccd
alulab14 <alulab14@inf.pucp.edu.pe>
2018-10-09 12:51:03 -0500

    modificaciones bd

--- ProyectoCsharp/AccesoDatos/DBManager.cs
+++ ProyectoCsharp/AccesoDatos/DBManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,78 @@ namespace AccesoDatos
 {
     public class DBManager
     {
-        public static string cadena = "server=quilla.lab.inf.pucp.edu.pe; user=inf282g2; password=0Fjw8H; database=a20145779; SslMode=None";
+        private static string cadena = "server=quilla.lab.inf.pucp.edu.pe; user=inf282g2; password=0Fjw8H; database=a20145779; SslMode=None";
+        private static MySqlConnection conexion = null;
+        private MySqlCommand cmd = null;
+        private MySqlDataAdapter adaptador = null;
+        private DataTable tabla = null;
 
+        public bool EjecutarProcedure(MySqlParameter[] parametrosEntrada, string nombProcedimiento)
+        {
+            try
+            {
+                conexion = new MySqlConnection(cadena);
+                cmd = new MySqlCommand(nombProcedimiento, conexion);
+                conexion.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddRange(parametrosEntrada);
+                cmd.ExecuteNonQuery();
+                conexion.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                conexion.Dispose();
+                cmd.Dispose();
+            }
+        }
 
+        public DataTable MostrarTablaDatos(string comando) //SELECT * FROM TABLA
+        {
+            try
+            {
+                conexion = new MySqlConnection(cadena);
+                tabla = new DataTable();
+                conexion.Open();
+                adaptador = new MySqlDataAdapter(comando, conexion);
+                adaptador.Fill(tabla);
+                conexion.Close();
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                //throw new Exception(ex.Message);
+                return null;
+            }
+            finally
+            {
+                conexion.Dispose();
+                //adaptador.Dispose();
+            }
+        }
+
+        /*public MySqlDataReader MostrarInformacion(string datos)
+        {
+            try
+            {
+                cmd = new MySqlCommand(datos, conexion);
+                conexion.Open();
+                leer = cmd.ExecuteReader();
+                return leer;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                conexion.Dispose();
+                cmd.Dispose();
+            }
+        }*/
     }
 }
--- ProyectoCsharp/AccesoDatos/UsuarioDA.cs
+++ ProyectoCsharp/AccesoDatos/UsuarioDA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace AccesoDatos
 {
     public class UsuarioDA
     {
+        private DBManager objManager;
+        MySqlParameter[] parametrosEntrada = null;
+
+        public UsuarioDA()
+        {
+            parametrosEntrada = new MySqlParameter[6];
+        }
     }
 }
