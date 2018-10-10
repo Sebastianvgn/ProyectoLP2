e5200b9894d36b345a6ef26862614cac2a29b27a
Sebastian Villa-García <43787661+Sebastianvgn@users.noreply.github.com>
2018-10-10 12:45:56 -0500

    mucho

---
+++ TrabajoJavaLp2/src/AccesoDatos/AlmacenDA.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AccesoDatos;
+
+import modelo.Almacen;
+
+/**
+ *
+ * @author alulab14
+ */
+public class AlmacenDA {
+    public void crearAlmacen(Almacen alm){
+        try{
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
--- TrabajoJavaLp2/src/modelo/Area.java
+++ TrabajoJavaLp2/src/modelo/Area.java
@@ -3,12 +3,30 @@ public class Area{
 	private long idArea;
 	private String nombreArea;
 
+    
+
 	public Area(){
-        idArea = 0;
-		nombreArea = null;
+            idArea = 0;
+            nombreArea = null;
 	}
 	public Area(long idArea, String nombreArea){
-        idArea = idArea;
-		nombreArea = nombreArea;
+            this.idArea = idArea;
+            this.nombreArea = nombreArea;
 	}
+        
+        public long getIdArea() {
+            return idArea;
+        }
+
+        public void setIdArea(long idArea) {
+            this.idArea = idArea;
+        }
+
+        public String getNombreArea() {
+            return nombreArea;
+        }
+
+        public void setNombreArea(String nombreArea) {
+            this.nombreArea = nombreArea;
+        }
 }
